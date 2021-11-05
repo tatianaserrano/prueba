583208cb9f581af5f9cf5e8ab1e9b15edd6288ae
tatianaserrano <tatianaserrano96@gmail.com>
2021-11-05 09:41:45 -0500

    add initial files

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+  res.send('Prueba del servidor');
+});
+
+app.listen(port, () => {
+  return console.log(`server is listening on ${port}`);
+});
